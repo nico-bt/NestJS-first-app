5acc5aa490184e57ead6a523d1dcb585fd1add78
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-16 17:05:12 -0300

    Import services as providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from 'src/student/student.controller';
+import { StudentService } from 'src/student/student.service';
 import { TeacherController } from 'src/teachers/teachers.controller';
+import { TeachersService } from 'src/teachers/teachers.service';
 
 @Module({
   imports: [],
   controllers: [StudentController, TeacherController],
-  providers: [],
+  providers: [StudentService, TeachersService],
 })
 export class AppModule {}
